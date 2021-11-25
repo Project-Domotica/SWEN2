59c0ceff21eda5eaa43d6931a3736aa177a5c8e0
joasonvlee <joas.onvlee@gmail.com>
2021-11-25 13:35:42 +0100

    Remove build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/joaso/Documents/School/Avans/Vakken/Software Engineering/SWEN2
-# Build directory: C:/Users/joaso/Documents/School/Avans/Vakken/Software Engineering/SWEN2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
