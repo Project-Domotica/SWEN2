6b1b59dd791a3574ea41d67af363e95a0a49e862
Max te Lintelo <max.te.lintelo@gmail.com>
2021-11-25 13:51:24 +0100

    Added help command and algorithm parameter

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,24 @@
 #include <string>
 #include <iostream>
 
+#define HELP_STR "usage: sort [--help] [--verbose]\n\t<--algorithm <algorithm>> <args>\n\navailable algorithms\n\tbubble\n\tmerge\n\theap\n"
+
 int main(int argc, char* argv[]) {
     argh::parser cmdl(argc, argv, argh::parser::PREFER_PARAM_FOR_UNREG_OPTION);
     bool verbose = cmdl["-v"];
-    if(verbose)
-        std::cout << "Hello" << std::endl;
+    
+    // if help was specified
+    if (cmdl[{ "-h", "--help" }])
+    {
+        std::cout << HELP_STR;
+        return 0;
+    }
+
+    // if no algorithm was specified
+    if (!(cmdl({ "-a", "--algorithm" })))
+    {
+        std::cout << HELP_STR << std::endl;
+        std::cout << "error: please specify an algorithm.";
+        return 0;
+    }
 }
