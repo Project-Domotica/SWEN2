68a9fd36fba7efdd848a2fba55934b56ea671e11
joasonvlee <joas.onvlee@gmail.com>
2021-11-25 13:32:43 +0100

    Update Cmake

---
+++ build/CMakeFiles/SWEN2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SWEN2.dir/main.cpp.obj"
+  "CMakeFiles/SWEN2.dir/main.cpp.obj.d"
+  "SWEN2.exe"
+  "SWEN2.exe.manifest"
+  "SWEN2.pdb"
+  "libSWEN2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SWEN2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/joaso/Documents/School/Avans/Vakken/Software Engineering/SWEN2
+# Build directory: C:/Users/joaso/Documents/School/Avans/Vakken/Software Engineering/SWEN2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
